8368b32e6be48df138c7e6058b6cd87a9afd73ed
sapna <nagwanshisapna@gmail.com>
2026-02-18 09:10:02 +0530

    Added Dynamic Route segments for Cabin page

---
+++ app/cabins/loading.js
@@ -0,0 +1,10 @@
+import Spinner from "@/app/_components/Spinner";
+
+export default function Loading() {
+  return (
+    <div className="grid items-center justify-center">
+      <Spinner />
+      <p className="text-primary-500">Loading Cabin Data</p>
+    </div>
+  );
+}
--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,12 +1,13 @@
-import CabinCard from "@/app/_components/CabinCard";
+import { Suspense } from "react";
+import CabinList from "../_components/CabinList";
+import Spinner from "../_components/Spinner";
 
 export const metadata = {
   title: "Cabins",
 };
 
-export default function Page() {
+export default async function Page() {
   // CHANGE
-  const cabins = [];
 
   return (
     <div>
@@ -22,13 +23,9 @@ export default function Page() {
         Welcome to paradise.
       </p>
 
-      {cabins.length > 0 && (
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-          {cabins.map((cabin) => (
-            <CabinCard cabin={cabin} key={cabin.id} />
-          ))}
-        </div>
-      )}
+      <Suspense fallback={<Spinner />}>
+        <CabinList />
+      </Suspense>
     </div>
   );
 }
