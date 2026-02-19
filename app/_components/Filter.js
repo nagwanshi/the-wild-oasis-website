0ff2f55326acacb939051474a318d079cbe8aea3
sapna <nagwanshisapna@gmail.com>
2026-02-19 10:19:36 +0530

    Highlight currently active filter

--- app/_components/Filter.js
+++ app/_components/Filter.js
@@ -2,10 +2,18 @@
 
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
+const filterButtons = [
+  { id: 1, text: " All", filter: "all" },
+  { id: 2, text: "1-3", filter: "small" },
+  { id: 3, text: "4-7", filter: "medium" },
+  { id: 4, text: "8-12", filter: "large" },
+];
+
 function Filter() {
   const searchParams = useSearchParams();
   const router = useRouter();
   const pathName = usePathname();
+  const activeFilter = searchParams.get("capacity") ?? "all";
 
   function handleFilter(filter) {
     const params = new URLSearchParams(searchParams);
@@ -14,31 +22,28 @@ function Filter() {
   }
   return (
     <div className="flex border border-primary-800">
-      <button
-        className="px-5 py-2 hover:bg-primary-700"
-        onClick={() => handleFilter("all")}
-      >
-        All
-      </button>
-      <button
-        className="px-5 py-2 hover:bg-primary-700"
-        onClick={() => handleFilter("small")}
-      >
-        1&mdash;3
-      </button>
-      <button
-        className="px-5 py-2 hover:bg-primary-700"
-        onClick={() => handleFilter("medium")}
-      >
-        4&mdash;7
-      </button>
-      <button
-        className="px-5 py-2 hover:bg-primary-700"
-        onClick={() => handleFilter("large")}
-      >
-        8&mdash;12
-      </button>
+      {filterButtons.map((btn) => (
+        <Button
+          key={btn.id}
+          filter={btn.filter}
+          activeFilter={activeFilter}
+          handleFilter={handleFilter}
+        >
+          {btn.text}
+        </Button>
+      ))}
     </div>
+  );
+}
+
+function Button({ filter, handleFilter, activeFilter, children }) {
+  return (
+    <button
+      className={`${activeFilter === filter ? "bg-primary-900" : ""} px-5 py-2 hover:bg-primary-700`}
+      onClick={() => handleFilter(filter)}
+    >
+      {children}
+    </button>
   );
 }
 
