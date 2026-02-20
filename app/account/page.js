50eee9a3d5682023eaf3b81462b7e7f11cc81d59
sapna <nagwanshisapna@gmail.com>
2026-02-20 15:21:07 +0530

    Add signout functionality

--- app/account/page.js
+++ app/account/page.js
@@ -1,12 +1,15 @@
+import { auth } from "../_lib/auth";
+
 export const metadata = {
   title: "Guest area",
 };
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
   return (
     <div>
       <h2 className="font-semibold text-2xl text-accent-400 mb-4">
-        Welcome Sapna!
+        {session.user.name}
       </h2>
     </div>
   );
