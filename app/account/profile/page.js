6bba6ba9f3802c69555344fcaccf94c30676bc68
sapna <nagwanshisapna@gmail.com>
2026-02-22 08:25:10 +0530

    Add loading state to profile form

--- app/account/profile/page.js
+++ app/account/profile/page.js
@@ -1,8 +1,10 @@
 import SelectCountry from "@/app/_components/SelectCountry";
+import Spinner from "@/app/_components/Spinner";
 import UpdateProfileForm from "@/app/_components/UpdateProfileForm";
 import { auth } from "@/app/_lib/auth";
 import { getGuest } from "@/app/_lib/data-service";
 import Image from "next/image";
+import { Suspense } from "react";
 
 export const metadata = {
   title: "Update Profile",
@@ -22,14 +24,16 @@ export default async function Page() {
         Providing the following information will make your check-in process
         faster and smoother. See you soon!
       </p>
-      <UpdateProfileForm guest={guest}>
-        <SelectCountry
-          name="nationality"
-          id="nationality"
-          className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
-          defaultCountry={guest.nationality}
-        />
-      </UpdateProfileForm>
+      <Suspense fallback={<Spinner />}>
+        <UpdateProfileForm guest={guest}>
+          <SelectCountry
+            name="nationality"
+            id="nationality"
+            className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
+            defaultCountry={guest.nationality}
+          />
+        </UpdateProfileForm>
+      </Suspense>
     </div>
   );
 }
